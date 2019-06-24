d76db20aaeb04d6c1dc94b858a50a674f719807c
Supriya954 <52148143+Supriya954@users.noreply.github.com>
2019-06-24 12:34:40 +0530

    Add files via upload

---
+++ FacebookUi.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App2
+{
+    public partial class FacebookUi : ContentPage
+    {
+        public FacebookUi(JObject json)
+        { 
+            InitializeComponent();
+            
+            Name.Text = json["name"].ToString();
+            JObject image = (JObject)json["picture"];
+            JObject url = (JObject)image["data"];
+
+            userimg.Source = url["url"].ToString();
+            idlabel.Text = json["id"].ToString();
+        }
+    }
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Xamarin.Forms;
+
+namespace App2
+{
+    public partial class MainPage : ContentPage
+    {
+        private string clientId = "2519521438109374";
+        string token = null;
+        string apiRequest = "";
+        public MainPage()
+        {
+            InitializeComponent();
+            apiRequest = "https://www.facebook.com/v3.3/dialog/oauth? client_id=" + clientId + "&response_type=token&redirect_uri=https://www.facebook.com/connect/login_success.html";
+
+        }
+
+        private async void WebViewOnNavigated(object sender, WebNavigatedEventArgs web)
+        {
+            if (web.Url.Contains("access_token"))
+            {
+                token = ExtractAccessTokenUrl(web.Url);
+                await GetFacebookProfile(token);
+            }
+          
+        }
+
+        private string ExtractAccessTokenUrl(string url)
+        {
+            string at = null;
+            if (url.Contains("access_token") && url.Contains("&expires_in"))
+                at = url.Replace("https://www.facebook.com/connect/login_success.html#access_token=", "");
+            return at;
+        }
+
+        public async Task GetFacebookProfile(string accessNo)
+        {
+             var requestUrl = "https://graph.facebook.com/v2.7/me?" + "fields=name,gender,picture,email,first_name,last_name,cover,birthday&access_token=" + accessNo;
+
+            HttpClient httpClient = new HttpClient();
+            var userJson = await httpClient.GetStringAsync(requestUrl);
+            string data = JsonConvert.DeserializeObject(userJson).ToString();
+            JObject parsed = JObject.Parse(data.ToString());
+
+            FacebookUi facebookUi = new FacebookUi(parsed);
+
+            JObject image = (JObject)parsed["picture"];
+            JObject url = (JObject)image["data"];
+            var webview = new WebView();
+            webview.GoBack();
+            Content = webview;
+            await Navigation.PushAsync(new FacebookUi(parsed));
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            var webview = new WebView()
+            {
+                Source = apiRequest,
+                HeightRequest = 1
+            };
+            webview.Navigated += WebViewOnNavigated;
+            Content = webview;
+        }
+    }
+}
